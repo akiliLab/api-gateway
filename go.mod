de52ed9423f420954b9cd0a9c52d0584aef49379
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-05-27 15:46:52 +0300

    Added handler for transactions using account id

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/akililab/api-gateway
 
 go 1.12
 
-require github.com/akililab/transaction v0.0.0-20190527103618-4151138e1fc3
+require (
+	github.com/akililab/transaction v0.0.0-20190527123217-30712114281f
+	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/micro/go-micro v1.1.0
+	github.com/micro/go-web v1.0.0
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	transaction "github.com/akililab/transaction/proto"
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-web"
 )
 
 // Server : Gateway struct
 type Server struct{}
 
 var (
-	cl transaction.TransactionService
+	ts transaction.TransactionService
 )
 
-// TransactionRequest : Transaction Request
-type TransactionRequest struct {
-	AccountID string `json:"account_id"`
+// GetTransactions : GetTransaction struct
+func (s *Server) GetTransactions(c *gin.Context) {
+
+	log.Print("Received Server.GetTransactions API request")
+
+	accountid := c.Param("accountid")
+
+	response, err := ts.GetTransactions(context.TODO(), &transaction.TransactionRequest{AccountId: accountid})
+
+	if err != nil {
+		c.JSON(500, err)
+	}
+
+	c.JSON(200, response)
+
 }
 
 func main() {
-	
+
+	// Create service
+	service := web.NewService(
+		web.Name("micro.akililab.api.gateway"),
+	)
+
+	service.Init()
+
+	// setup Greeter Server Client
+	ts = transaction.NewTransactionService("micro.akililab.transaction", client.DefaultClient)
+
+	// Create RESTful handler (using Gin)
+
+	server := new(Server)
+	router := gin.Default()
+	router.GET("/transactions/:accountid", server.GetTransactions)
+
+	// Register Handler
+	service.Handle("/", router)
+
+	// Run server
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+
 }
