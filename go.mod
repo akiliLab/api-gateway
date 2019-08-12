8c50d8d19ab3fd14754305cbd394d928c5bfc7b9
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-08-12 14:32:42 +0300

    just a test for getting greeting

--- go.mod
+++ go.mod
@@ -3,13 +3,11 @@ module github.com/akililab/api-gateway
 go 1.12
 
 require (
+	github.com/akiliLab/account v0.0.0-20190812112113-c6c3b9922eb8
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/golang/protobuf v1.3.1
-	github.com/grpc-ecosystem/grpc-gateway v1.9.3
+	github.com/golang/protobuf v1.3.2
+	github.com/grpc-ecosystem/grpc-gateway v1.9.5
 	github.com/nicholasjackson/env v0.5.0
-	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
-	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
-	google.golang.org/genproto v0.0.0-20190307195333-5fe7a883aa19
-	google.golang.org/grpc v1.21.0
+	google.golang.org/genproto v0.0.0-20190801165951-fa694d86fc64
+	google.golang.org/grpc v1.22.1
 )
--- main.go
+++ main.go
@@ -1,45 +1,95 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
+	"os"
 
-	"context"
-
-	"github.com/golang/glog"
+	gw "github.com/akiliLab/account/proto"
+	lrf "github.com/banzaicloud/logrus-runtime-formatter"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
-
-	balance "github.com/akililab/api-gateway/proto/balance"
+	"google.golang.org/grpc/metadata"
 )
 
-var (
-	// the go.micro.srv.balance address
-	endpoint = flag.String("endpoint", "balance.default:9080", "go.micro.srv.balance address")
-)
+func injectHeadersIntoMetadata(ctx context.Context, req *http.Request) metadata.MD {
+	//https://aspenmesh.io/2018/04/tracing-grpc-with-istio/
+	var (
+		otHeaders = []string{
+			"x-request-id",
+			"x-b3-traceid",
+			"x-b3-spanid",
+			"x-b3-parentspanid",
+			"x-b3-sampled",
+			"x-b3-flags",
+			"x-ot-span-context"}
+	)
+	var pairs []string
+
+	for _, h := range otHeaders {
+		if v := req.Header.Get(h); len(v) > 0 {
+			pairs = append(pairs, h, v)
+		}
+	}
+	return metadata.Pairs(pairs...)
+}
+
+type annotator func(context.Context, *http.Request) metadata.MD
+
+func chainGrpcAnnotators(annotators ...annotator) annotator {
+	return func(c context.Context, r *http.Request) metadata.MD {
+		var mds []metadata.MD
+		for _, a := range annotators {
+			mds = append(mds, a(c, r))
+		}
+		return metadata.Join(mds...)
+	}
+}
 
 func run() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithInsecure()}
+	annotators := []annotator{injectHeadersIntoMetadata}
 
-	err := balance.RegisterBalanceHandlerFromEndpoint(ctx, mux, *endpoint, opts)
+	mux := runtime.NewServeMux(
+		runtime.WithMetadata(chainGrpcAnnotators(annotators...)),
+	)
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterGreetingServiceHandlerFromEndpoint(ctx, mux, "localhost:50051", opts)
 	if err != nil {
 		return err
 	}
+	return http.ListenAndServe(":80", mux)
+}
 
-	return http.ListenAndServe(":8080", mux)
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func init() {
+	formatter := lrf.Formatter{ChildFormatter: &log.JSONFormatter{}}
+	formatter.Line = true
+	log.SetFormatter(&formatter)
+	log.SetOutput(os.Stdout)
+	level, err := log.ParseLevel(getEnv("LOG_LEVEL", "info"))
+	if err != nil {
+		log.Error(err)
+	}
+	log.SetLevel(level)
 }
 
 func main() {
 	flag.Parse()
 
-	defer glog.Flush()
-
 	if err := run(); err != nil {
-		glog.Fatal(err)
+		log.Fatal(err)
 	}
 }
